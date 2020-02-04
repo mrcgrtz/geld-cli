5a0256f450efd71e2ad4d135704015633b9ca0bd
Marc Görtz <mail@marcgoertz.de>
2020-02-04 12:34:52 +0100

    Fix tests.

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
-import test from 'ava';
-import execa from 'execa';
+const test = require('ava');
+const execa = require('execa');
 
 test('Input without flags and value', async t => {
 	const error = await t.throwsAsync(() => execa('./cli.js'));
